3c9dc5e26b2de9b5a66e93a0d17528b8cf459cae
achugo <cachugo@infomall.ng>
2020-02-28 15:16:52 +0100

    overwrite all files

---
+++ js/navbar1.js
@@ -0,0 +1,55 @@
+(function($) {
+  // Begin jQuery
+  $(document).ready(function() {
+    // DOM ready
+    // If a link has a dropdown, add sub menu toggle.
+    $("nav ul li a:not(:only-child)").click(function(e) {
+      $(this)
+        .siblings(".nav-dropdown")
+        .toggle();
+      // Close one dropdown when selecting another
+      $(".nav-dropdown")
+        .not($(this).siblings())
+        .hide();
+      e.stopPropagation();
+    });
+    // Clicking away from dropdown will remove the dropdown class
+    $("html").click(function() {
+      $(".nav-dropdown").hide();
+    });
+    // Toggle open and close nav styles on click
+    $("#nav-toggle1").click(function() {
+      if ($(".x-men").length == 0) {
+        $("#nav-toggle1").addClass("x-men");
+        $("nav ul").fadeIn("fast");
+      } else {
+        $("nav ul").fadeOut("fast");
+      }
+    });
+
+    // $("#nav-toggle1").click(function() {
+    //   if ($(".x-men").length == 0) {
+    //     $("#nav-toggle1").addClass("x-men");
+    //     $("nav ul").fadeIn("fast");
+    //   } else {
+    //     $("nav ul").fadeOut("fast");
+    //   }
+    // });
+
+    $(".linkM").click(function() {
+      if (window.matchMedia("(max-width: 700px)").matches) {
+        $("#nav-toggle1").removeClass("x-men");
+        $("#nav-toggle1").removeClass("active");
+        $("nav ul").fadeOut("fast");
+      }
+    });
+
+    // Hamburger to X toggle
+    $("#nav-toggle1").on("click", function() {
+      if (this.classList.contains("active")) {
+        this.classList.remove("x-men");
+      }
+      this.classList.toggle("active");
+    });
+  }); // end DOM ready
+})(jQuery); // end jQuery
---
+++ precache-manifest.552b05f058d89a08dd7c7082b95783fa.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbc5173b80c8de2ef615e4cf804d4dbf",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d631e17ad10feb44cc31",
+    "url": "./static/css/main.3f568e7b.chunk.css"
+  },
+  {
+    "revision": "4ca68fa7c8f931bf639f",
+    "url": "./static/js/2.3c8cd43f.chunk.js"
+  },
+  {
+    "revision": "d631e17ad10feb44cc31",
+    "url": "./static/js/main.3884dd5c.chunk.js"
+  },
+  {
+    "revision": "7e79b53da7ec2bb38f4d",
+    "url": "./static/js/runtime-main.cfbc7f7d.js"
+  },
+  {
+    "revision": "e41a7714c07c8ac3a18faf60f8e593e3",
+    "url": "./static/media/Avenir-Black-webfont.e41a7714.woff"
+  },
+  {
+    "revision": "019c82a4f9b1a9898c0b8239a1ddb3e2",
+    "url": "./static/media/Avenir-Medium-webfont.019c82a4.woff"
+  },
+  {
+    "revision": "ea7b7bc954f573827e2143f8153fcb0e",
+    "url": "./static/media/Avenir-Roman-webfont.ea7b7bc9.woff"
+  },
+  {
+    "revision": "50145685042b4df07a1fd19957275b81",
+    "url": "./static/media/OpenSans-Bold.50145685.ttf"
+  },
+  {
+    "revision": "1bf71be111189e76987a4bb9b3115cb7",
+    "url": "./static/media/OpenSans-Light.1bf71be1.ttf"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "./static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "33f225b8f5f7d6b34a0926f58f96c1e9",
+    "url": "./static/media/OpenSans-Semibold.33f225b8.ttf"
+  },
+  {
+    "revision": "738d0a0b02de37eb7d389ba8a6da2bf1",
+    "url": "./static/media/WorkSans-Light.738d0a0b.otf"
+  },
+  {
+    "revision": "27ac30b0618e216641800fea309585b5",
+    "url": "./static/media/WorkSans-SemiBold.27ac30b0.otf"
+  },
+  {
+    "revision": "e7ad340e7acf8586b435139c8b1390c7",
+    "url": "./static/media/avenir-light-webfont.e7ad340e.woff"
+  }
+]);
